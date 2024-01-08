ee763498171f170573e6a56b4868f94f680961ea
codetalks95 <info@codetalksdna.com>
2024-01-08 09:18:43 +0530

    My First Code

---
+++ src/main/java/com/sheetal/sheetal_springboot_project/SheetalSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sheetal.sheetal_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetalSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetalSpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sheetal/sheetal_springboot_project/SheetalSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheetal.sheetal_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetalSpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
