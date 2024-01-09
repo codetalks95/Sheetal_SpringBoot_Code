240ac8b49ff9a616ae62ee87bebcd46e87850b64
codetalks95 <info@codetalksdna.com>
2024-01-09 09:00:36 +0530

    New branch added.

--- src/main/java/com/sheetal/sheetal_springboot_project/Test.java
+++ src/main/java/com/sheetal/sheetal_springboot_project/Test.java
@@ -2,6 +2,6 @@ package com.sheetal.sheetal_springboot_project;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("welcoem to java");
+        System.out.println("Added new branch to Prod");
     }
 }
