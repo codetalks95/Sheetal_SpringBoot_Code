3c677121bc784a52c4f7fe2f84cccfd91b97a451
codetalks95 <info@codetalksdna.com>
2024-01-13 11:40:14 +0530

    committed

--- src/main/java/com/sheetal/sheetal_springboot_project/MilkRateController.java
+++ src/main/java/com/sheetal/sheetal_springboot_project/MilkRateController.java
@@ -2,7 +2,6 @@ package com.sheetal.sheetal_springboot_project;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
