c26d3dba753fc1da984d1584a918b4ee034bc7e5
codetalks95 <info@codetalksdna.com>
2024-02-14 21:36:21 +0530

    Added and Fixed Issue with h2 Database.

--- src/main/java/com/sheetal/sheetal_springboot_project/security/SecurityConfig.java
+++ src/main/java/com/sheetal/sheetal_springboot_project/security/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.sheetal.sheetal_springboot_project.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,11 +17,13 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails admin = User.withUsername("sheetal")
@@ -32,25 +37,23 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(admin, user);
     }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http.csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth ->
-//                        auth.requestMatchers("/**").hasAnyRole("ADMIN","USER")
-//                                .requestMatchers("/**")
-//                                .authenticated())
-//                .httpBasic(Customizer.withDefaults()).build();
-//    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/**").permitAll();
-//                    auth.requestMatchers("/").permitAll(); #use if you wish to use the authentication in this case is Oauth2..
-                    auth.anyRequest().authenticated();
-                }).oauth2Login(withDefaults()).
-                formLogin(withDefaults()).
-                build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(antMatcher("/swagger-ui/**"),
+                                    antMatcher("/swagger-ui.html"),
+                                    antMatcher("/v3/**"),
+                                    antMatcher("/h2-console/**")).permitAll()
+                            .requestMatchers(antMatcher("/")).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // This so embedded frames in h2-console are working
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .build();
     }
 
     @Bean
@@ -58,3 +61,5 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+
+
